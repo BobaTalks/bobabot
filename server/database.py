190a0d064898f621d8ed424f076f441663c85b3b
Joshua Wu <joshua@pepperdata.com>
2023-04-19 17:10:25 -0700

    ci: set up github action for black linting

--- server/database.py
+++ server/database.py
@@ -5,16 +5,17 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-MONGO_URI = os.environ['MONGO_URI']
-DATABASE_NAME = os.environ['DATABASE_NAME']
+MONGO_URI = os.environ["MONGO_URI"]
+DATABASE_NAME = os.environ["DATABASE_NAME"]
+
 
 def initialize_database():
     client = MongoClient(MONGO_URI)
     database = client.DATABASE_NAME
-    
-    #create tags and reviewers collections if they do not already exist
+
+    # create tags and reviewers collections if they do not already exist
     database.tags
-    
+
     database.tags.create_index("snowflake_id", unique=True)
     # expected structure for a tag:
     # {
@@ -26,35 +27,38 @@ def initialize_database():
 
     return database
 
+
 # create_tag inserts a tag object into the database
 def create_tag(db, data):
     try:
         db.tags.insert_one(data)
     except errors.DuplicateKeyError:
-        return ({"Error":"Resource Already Exists"}, 400)
+        return ({"Error": "Resource Already Exists"}, 400)
     return ("Created", 201)
+
 
 # read_tags retrieves all of the tag objects in the database
 def read_tags(db):
     # get mongo db object and convert to string
     mongo_string = dumps(db.tags.find())
-    
+
     # encode string to json
     payload = json.loads(mongo_string)
     return (payload, 200)
 
+
 # read_tag_by_id retrieves the tag object with the given id
 def read_tag_by_id(db, id):
-    mongo_string = dumps(db.tags.find_one({'snowflake_id': id}))
+    mongo_string = dumps(db.tags.find_one({"snowflake_id": id}))
     if mongo_string == "null":
         return ({"Error": "Resource Does Not Exist"}, 404)
     payload = json.loads(mongo_string)
     return (payload, 200)
 
+
 # delete_tag_by_id removes a tag from the database with the given id
 def delete_tag_by_id(db, id):
-    mongo_string = dumps(db.tags.find_one_and_delete({'snowflake_id': id}))
+    mongo_string = dumps(db.tags.find_one_and_delete({"snowflake_id": id}))
     if mongo_string == "null":
         return ({"Error": "Resource Does Not Exist"}, 404)
     return ("Delete Successful", 200)
-
