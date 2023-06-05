82bb79ef900c2bfbd895f053ec2215f5429213be
Anthony Camarillo <anthony.camarillo.96@gmail.com>
2023-06-05 16:23:15 -0700

    add documentation for bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -19,12 +19,35 @@ bot = commands.Bot(command_prefix=("?",), intents=intents)
 
 
 def get_thread_tags(ctx, channel_name):
+    """
+    Given the discord forum channel name, gets the tags applied to a forum thread
+
+    Parameters
+    ----------
+    ctx : discord.ext.commands.Context
+        An object that represents the context in which the discord command is invoked
+    channel_name: str
+        The name for the respective forum channel
+    """
     if ctx.parent.name == channel_name:
         channel_tags = ctx.applied_tags
         return channel_tags
 
 
 def get_forum_tags(client, guild_name, channel_name):
+    """
+    Given the discord server name AND forum channel name, gets all of the
+    tags available to that forum
+
+    Parameters
+    ----------
+    client : discord.ext.commands.Bot
+        A discord bot object
+    guild_name : str
+        The name for the respective server(known as guilds in Discord API)
+    channel_name: str
+        The name for the respective forum channel
+    """
     client_guilds = client.guilds
     client_channels = get(client_guilds, name=guild_name).channels
     forum_tags = get(client_channels, name=channel_name).available_tags
@@ -32,6 +55,14 @@ def get_forum_tags(client, guild_name, channel_name):
 
 
 def create_message_string(iter_tags):
+    """
+    Creates a comma separated string from an iterable of forum tags
+
+    Parameters
+    ----------
+    iter_tags : Sequence[ForumTag]
+        An iterable of Discord Forum Tags
+    """
     tags = [tag.name for tag in iter_tags]
     return ", ".join(tags)
 
@@ -43,6 +74,15 @@ async def on_ready():
 
 @bot.tree.command()
 async def list_tags(interaction):
+    """
+    Lists all of the available tags.
+
+    Parameters
+    ----------
+    interaction: discord.Interaction
+        The action implemented by the user that needs to be notified. 
+        In the context of the bot, the action is a slash command
+    """
     forum_tags = get_forum_tags(bot, server_name, channel_name)
     message_string = create_message_string(forum_tags)
     await interaction.response.send_message(f"The available tags are {message_string}")
@@ -50,12 +90,30 @@ async def list_tags(interaction):
 
 @bot.tree.command(name="mention")
 async def mention_me(interaction):
+    """
+    Sends a message mentioning the user who called this function.
+
+    Parameters
+    ----------
+    interaction: discord.Interaction
+        The action implemented by the user that needs to be notified. 
+        In the context of the bot, the action is a slash command
+    """
     caller = interaction.user.id
     await interaction.response.send_message(f"Hello <@{caller}>, here is your mention")
 
 
 @bot.tree.command(name="opt_in")
 async def opt_in(interaction):
+    """
+    Presents a selectable menu of tags for reviewers to subscribe to.
+
+    Parameters
+    ----------
+    interaction: discord.Interaction
+        The action implemented by the user that needs to be notified. 
+        In the context of the bot, the action is a slash command
+    """
     forum_tags = get_forum_tags(bot, server_name, channel_name)
     view = MenuView()
     view.add_menu(forum_tags)
@@ -64,6 +122,14 @@ async def opt_in(interaction):
 
 @bot.event
 async def on_thread_create(ctx):
+    """
+    On creation of a thread, sends a message listing all of the applied tags.
+
+    Parameters
+    ----------
+    ctx : discord.ext.commands.Context
+        An object that represents the context in which the discord command is invoked
+    """
     channel_tags = get_thread_tags(ctx, channel_name)
     message_string = create_message_string(channel_tags)
     await ctx.send(f"The applied tags are: {message_string}")
