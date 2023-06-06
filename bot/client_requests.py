3507afa6b73ff4cf57234c8da6d666187a49704a
a-camarillo <anthony.camarillo.96@gmail.com>
2023-06-06 11:06:55 -0700

    (feat): add client post request for tags to db

---
+++ bot/client_requests.py
@@ -0,0 +1,30 @@
+"""
+client_requests contains all of the functionality for the bot client to
+make requests to the server
+"""
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+server_url = os.environ["SERVER_URL"]
+
+
+def sync_all_tags(forum_tags):
+    """
+    Makes a post request to the server's /tags endpoint sending all of
+    the channel's available tags. This should only be called when syncing
+    the bot's commands
+
+    Parameters
+    ----------
+    forum_tags : Sequence[ForumTag]
+        An iterable of all Discord Forum Tags for the specified channel
+    """
+    for tag in forum_tags:
+        tag_dict = {}
+        tag_dict["snowflake_id"] = tag.id
+        tag_dict["name"] = tag.name
+        tag_dict["subscribers"] = []
+        requests.post(f"{server_url}/tags", json=tag_dict)
